26160d0e1dae95eeaed3ff60cc3dec9b669939a1
benjaaamin0518 <nishikawa.1101.sub@gmail.com>
2026-03-08 11:09:32 +0900

    [fix] verを修正

--- discogs-userscript.user.js
+++ discogs-userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Discogs Price Helper
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  Show Discogs price ranges on Mercari and Yahoo Auctions listings
 // @author       You
 // @match        https://jp.mercari.com/item/*
